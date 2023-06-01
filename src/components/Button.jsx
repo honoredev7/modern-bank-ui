f1db204c5939dbe910240f3a5786e42cd96f0d09
honoré <honoreadj7@gmail.com>
2023-06-01 18:25:31 +0000

    initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+const Button = ({ style }) => (
+  <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${style} rounded-[10px]`}>
+    Get Started
+  </button>
+)
+
+export default Button
